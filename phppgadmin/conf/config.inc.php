88c8d8d9e2c26d9ff440fdf91dabe34bac52cf0d
Franco Valerio <franco.gvp@hotmail.com>
2015-11-26 13:27:21 -0300

    Revert "Probando otra IP y puerto para PostgreSQL"
    This reverts commit 6894d10ab511786b39037dc4f9d1b96bff6ade51.

--- phppgadmin/conf/config.inc.php
+++ phppgadmin/conf/config.inc.php
@@ -15,10 +15,10 @@
 
 	// Hostname or IP address for server.  Use '' for UNIX domain socket.
 	// use 'localhost' for TCP/IP connection on this computer
-	$conf['servers'][0]['host'] = '127.7.74.2';
+	$conf['servers'][0]['host'] = '127.0.0.1';
 
 	// Database port on server (5432 is the PostgreSQL default)
-	$conf['servers'][0]['port'] = 5432;
+	$conf['servers'][0]['port'] = 5433;
 
 	// Database SSL mode
 	// Possible options: disable, allow, prefer, require
