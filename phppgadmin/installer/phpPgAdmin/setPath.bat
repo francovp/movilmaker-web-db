39a6ae749e9a7bb255d71df4144cc58ace8995c2
Franco Valerio <franco.gvp@hotmail.com>
2015-11-25 23:57:58 -0300

    Añadiendo phpPgAdmin

---
+++ phppgadmin/installer/phpPgAdmin/setPath.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+REM Copyright (c) 2012, EnterpriseDB Corporation.  All rights reserved
+"C:/Program Files (x86)/PostgreSQL/EnterpriseDB-ApachePHP\php\php.exe" -r "echo addcslashes('C:\Program Files\PostgreSQL\9.4', '\\');"
